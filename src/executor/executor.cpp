a48356a28041024270dcdb01f72f427dc26ace69
Kieroxide <kieranbprivate:gmail.com>
2025-10-05 22:49:18 +0100

    Updated executor to use new structure

--- src/executor/executor.cpp
+++ src/executor/executor.cpp
@@ -10,31 +10,18 @@ using namespace std;
 namespace Executor {
 
 int executeFile(std::string filePath) {
-    std::string fileContents = utility::readFile(filePath);
-    auto lines = utility::splitByNewline(fileContents);
+    string fileContents = utility::readFile(filePath);
+    string fileContentsFormatted = utility::convertTabs(fileContents);
 
-    auto* interpreter = new Interpreter();
+    auto lexer = new Lexer();
+    auto tokens = lexer->tokenize(fileContentsFormatted);
 
-    int lineNumber = 1;
+    auto parser = new Parser();
+    auto ast = parser->parseProgram(tokens);
 
-    auto main = std::make_unique<parser::Node>();
-    main->type = parser::NodeType::PROGRAM;
-    main->value = "MAIN";
+    auto interpreter = new Interpreter();
+    interpreter->evaluate(ast);
 
-    // Loops through all lines and adds to main AST tree for interpreting
-    for (int i = 0; i < lines.size(); i++) {
-        auto block = utility::getBlock(lines, i, lineNumber);
-
-        vector<lexer::Token> tokens = lexer::tokenize(block, lineNumber);
-        unique_ptr<parser::Node> statement = parser::parseStatement(tokens);
-        main->addChild(std::move(statement));
-
-        lineNumber++;
-    }
-
-    interpreter->evaluate(main);
-    
-    delete interpreter;
     return 0;
 }
 }  // namespace Executor
