f32a32d1bee89bc79fcf5b586b573c90d4cbe3e4
Kieroxide <kieranbprivate:gmail.com>
2025-10-05 22:47:57 +0100

    Refactored parser to parse whole program in one. Also changed to a class. Embedded ifs now work!!!!

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -5,147 +5,176 @@
 #include <sstream>
 #include <string>
 
-namespace parser {
+using namespace std;
 
-std::unique_ptr<Node> parseStatement(const std::vector<lexer::Token>& tokens) {
-    int len = tokens.size();
-    int pos = 0;
+unique_ptr<Node> Parser::parseProgram(const vector<Token>& tokens) {
+    tokenPosition = 0;
+    tokenLength = tokens.size();
 
-    while (pos < len) {
-        lexer::Token token = tokens[pos++];
-        if (token.type == lexer::TokenType::PRINT) {
-            auto printStmt = std::make_unique<Node>();
+    const Token start = tokens[tokenPosition++];
+    if (tokenPosition >= tokenLength && start.type != TokenType::BLOCK) {
+        cerr << "Invalid Start to Program -- No Block Token";
+    }
+
+    unique_ptr<Node> program = make_unique<Node>();
+    program->type = NodeType::PROGRAM;
+    program->token = start;
+    program->value = "Program";
+
+    while (tokenPosition < tokenLength) {
+        unique_ptr<Node> statement = parseStatement(tokens);
+        // ignore nullptr statements these will be tokens that are not needed in AST
+        if (statement) {
+            program->addChild(move(statement));
+        }
+    }
+    return program;
+}
+
+unique_ptr<Node> Parser::parseStatement(const vector<Token>& tokens) {
+    Token token = tokens[tokenPosition++];
+    switch (token.type) {
+        case TokenType::PRINT: {
+            auto printStmt = make_unique<Node>();
 
             printStmt->type = NodeType::PRINT;
             printStmt->token = token;
             printStmt->value = "PRINT";
 
-            auto statement = parsePrint(tokens, pos);
-            printStmt->addChild(std::move(statement));
+            unique_ptr<Node> statement = parsePrint(tokens);
+            printStmt->addChild(move(statement));
             return printStmt;
-        } else if (token.type == lexer::TokenType::IDENTIFIER) {
-            auto identifier = std::make_unique<Node>();
+        }
+        case TokenType::IDENTIFIER: {
+            auto identifier = make_unique<Node>();
             identifier->token = token;
             identifier->type = NodeType::ASSIGN;
-            auto statement = parseIdentifier(tokens, pos);
-            identifier->addChild(std::move(statement));
+            unique_ptr<Node> statement = parseIdentifier(tokens);
+            identifier->addChild(move(statement));
             return identifier;
-        } else if (token.type == lexer::TokenType::IF) {
-            auto ifStmt = parseIf(tokens, pos);
+        }
+        case TokenType::IF: {
+            auto ifStmt = parseIf(tokens);
             return ifStmt;
         }
+        default:
+            return nullptr;
     }
 }
 
-std::unique_ptr<Node> parseIf(const std::vector<lexer::Token>& tokens, int& pos) {
-    pos--;
-    auto ifStmt = std::make_unique<Node>();
-    ifStmt->token = tokens[pos++];
+unique_ptr<Node> Parser::parseIf(const vector<Token>& tokens) {
+    auto ifStmt = make_unique<Node>();
+    const int ifPosition = tokenPosition - 1;  // Token for if is one behind pos
+    ifStmt->token = tokens[ifPosition];
     ifStmt->type = NodeType::IF;
     ifStmt->value = ifStmt->token.value;
 
-    auto condition = parseConditional(tokens, pos);
-    ifStmt->addChild(std::move(condition));
+    auto condition = parseConditional(tokens);
+    ifStmt->addChild(move(condition));
 
-    ++pos;  // for colon
+    if(tokens[tokenPosition++].type != TokenType::COLON){
+        cerr << "Expecting Colon";
+    }
+    while (tokenPosition < (int)tokens.size() && tokens[tokenPosition].type == TokenType::NEWLINE) ++tokenPosition;
 
-    auto block = std::make_unique<Node>();
-    block->token = tokens[pos++];
+    auto block = make_unique<Node>();
+    block->token = tokens[tokenPosition];
     block->type = NodeType::BLOCK;
     block->value = "BLOCK";
 
-    pos--;
-    while (pos < tokens.size() && tokens[pos].type == lexer::TokenType::INDENT) {
-        pos++;
-        std::vector<lexer::Token> tokenLine;
-        // Collects tokens until next INDENT or end
-        while (pos < tokens.size() && tokens[pos].type != lexer::TokenType::INDENT) {
-            tokenLine.push_back(tokens[pos++]);
-        }
-        if (!tokenLine.empty()) {
-            auto child = parseStatement(tokenLine);
-            block->addChild(std::move(child));
+    // Loops over all concurrent indents
+    while (tokenPosition < tokens.size() && tokens[tokenPosition].type == TokenType::INDENT) {
+        ++tokenPosition;  // consume indent
+
+        auto child = parseStatement(tokens);
+        if (child) block->addChild(move(child));
+
+        // Consumes trailing new lines
+        while (tokenPosition < (int)tokens.size() && tokens[tokenPosition].type == TokenType::NEWLINE) {
+            ++tokenPosition;
         }
     }
-    ifStmt->addChild(std::move(block));
+    ifStmt->addChild(move(block));
     return ifStmt;
 }
-std::unique_ptr<Node> parseConditional(const std::vector<lexer::Token>& tokens, int& pos) {
-    auto node = parseExpression(tokens, pos);
-    if (pos < tokens.size() &&
-        (tokens[pos].type == lexer::TokenType::EQUALS || tokens[pos].type == lexer::TokenType::GREATERTHAN ||
-         tokens[pos].type == lexer::TokenType::LESSTHAN)) {
-        auto conditional = std::make_unique<Node>();
-        conditional->token = tokens[pos++];
+
+unique_ptr<Node> Parser::parseConditional(const vector<Token>& tokens) {
+    auto node = parseExpression(tokens);
+    if (tokenPosition < tokens.size() &&
+        (tokens[tokenPosition].type == TokenType::EQUALS || tokens[tokenPosition].type == TokenType::GREATERTHAN ||
+         tokens[tokenPosition].type == TokenType::LESSTHAN)) {
+        auto conditional = make_unique<Node>();
+        conditional->token = tokens[tokenPosition++];
         conditional->type = NodeType::CONDITIONAL;
         conditional->value = conditional->token.value;
 
-        auto right = parseExpression(tokens, pos);
-        conditional->addChild(std::move(node));
-        conditional->addChild(std::move(right));
-        node = std::move(conditional);
+        auto right = parseExpression(tokens);
+        conditional->addChild(move(node));
+        conditional->addChild(move(right));
+        node = move(conditional);
     }
     return node;
 }
-std::unique_ptr<Node> parseIdentifier(const std::vector<lexer::Token>& tokens, int& pos) {
-    lexer::Token token = tokens[pos++];
-    return parseExpression(tokens, pos);
+unique_ptr<Node> Parser::parseIdentifier(const vector<Token>& tokens) {
+    Token token = tokens[tokenPosition++];
+    return parseExpression(tokens);
 }
-std::unique_ptr<Node> parsePrint(const std::vector<lexer::Token>& tokens, int& pos) {
-    std::unique_ptr<Node> node = parseExpression(tokens, pos);
+unique_ptr<Node> Parser::parsePrint(const vector<Token>& tokens) {
+    unique_ptr<Node> node = parseExpression(tokens);
+    ++tokenPosition; // consumes colon
     return node;
 }
 
-std::unique_ptr<Node> parseExpression(const std::vector<lexer::Token>& tokens, int& pos) {
-    auto node = parseTerm(tokens, pos);
-    while (pos < tokens.size() &&
-           (tokens[pos].type == lexer::TokenType::PLUS || tokens[pos].type == lexer::TokenType::SUBTRACT)) {
-        lexer::Token token = tokens[pos++];
+unique_ptr<Node> Parser::parseExpression(const vector<Token>& tokens) {
+    auto node = parseTerm(tokens);
+    while (tokenPosition < tokens.size() &&
+           (tokens[tokenPosition].type == TokenType::PLUS || tokens[tokenPosition].type == TokenType::SUBTRACT)) {
+        Token token = tokens[tokenPosition++];
 
-        auto op = std::make_unique<Node>();
+        auto op = make_unique<Node>();
         op->token = token;
         op->type = NodeType::OPERATOR;
         op->value = token.value;
 
-        auto right = parseTerm(tokens, pos);
+        auto right = parseTerm(tokens);
 
-        op->addChild(std::move(node));
-        op->addChild(std::move(right));
-        node = std::move(op);
+        op->addChild(move(node));
+        op->addChild(move(right));
+        node = move(op);
     }
     return node;
 }
 
-std::unique_ptr<Node> parseTerm(const std::vector<lexer::Token>& tokens, int& pos) {
-    auto node = parseFactor(tokens, pos);
-    while (pos < tokens.size() &&
-           (tokens[pos].type == lexer::TokenType::MULTIPLY || tokens[pos].type == lexer::TokenType::DIVIDE)) {
-        lexer::Token token = tokens[pos++];
+unique_ptr<Node> Parser::parseTerm(const vector<Token>& tokens) {
+    auto node = parseFactor(tokens);
+    while (tokenPosition < tokens.size() &&
+           (tokens[tokenPosition].type == TokenType::MULTIPLY || tokens[tokenPosition].type == TokenType::DIVIDE)) {
+        Token token = tokens[tokenPosition++];
 
-        auto op = std::make_unique<Node>();
+        auto op = make_unique<Node>();
         op->token = token;
         op->type = NodeType::OPERATOR;
         op->value = token.value;
 
-        auto right = parseFactor(tokens, pos);
+        auto right = parseFactor(tokens);
 
-        op->addChild(std::move(node));
-        op->addChild(std::move(right));
-        node = std::move(op);
+        op->addChild(move(node));
+        op->addChild(move(right));
+        node = move(op);
     }
     return node;
 }
 
-std::unique_ptr<Node> parseFactor(const std::vector<lexer::Token>& tokens, int& pos) {
-    lexer::Token token = tokens[pos++];
-    if (token.type == lexer::TokenType::NUMBER) {
-        auto number = std::make_unique<Node>();
+unique_ptr<Node> Parser::parseFactor(const vector<Token>& tokens) {
+    Token token = tokens[tokenPosition++];
+    if (token.type == TokenType::NUMBER) {
+        auto number = make_unique<Node>();
         number->token = token;
         number->type = NodeType::NUMBER;
         number->value = token.value;
         return number;
-    } else if (token.type == lexer::TokenType::IDENTIFIER) {
-        auto variable = std::make_unique<Node>();
+    } else if (token.type == TokenType::IDENTIFIER) {
+        auto variable = make_unique<Node>();
         variable->token = token;
         variable->type = NodeType::VARIABLE;
         variable->value = token.value;
@@ -153,17 +182,16 @@ std::unique_ptr<Node> parseFactor(const std::vector<lexer::Token>& tokens, int& 
     }
 }
 
-void printAST(const std::unique_ptr<Node>& node, int indent) {
+void Parser::printAST(const unique_ptr<Node>& node, int indent) {
     if (!node)
         return;
 
-    std::string indentation(indent, ' ');
+    string indentation(indent, ' ');
 
-    std::cout << indentation << "NodeType: " << static_cast<int>(node->type) << ", Token: " << node->token.value
-              << ", Value: " << node->value << "\n";
+    cout << indentation << "NodeType: " << static_cast<int>(node->type) << ", Token: " << node->token.value
+         << ", Value: " << node->value << "\n";
 
     for (const auto& child : node->children) {
         printAST(child, indent + 2);
     }
 }
-}  // namespace parser
--- src/parser/parser.hpp
+++ src/parser/parser.hpp
@@ -3,15 +3,13 @@
 #include <string>
 #include <vector>
 
-#include "../lexer/lexer.hpp"
-
-namespace parser {
+#include "src/lexer/lexer.hpp"
 
 enum class NodeType { PROGRAM, ASSIGN, PRINT, BLOCK, IF, CONDITIONAL, OPERATOR, VARIABLE, NUMBER };
 
 struct Node {
     NodeType type;
-    lexer::Token token;
+    Token token;
 
     std::vector<std::unique_ptr<Node>> children;
 
@@ -22,13 +20,23 @@ struct Node {
     }
 };
 
-std::unique_ptr<Node> parseStatement(const std::vector<lexer::Token>& tokens);
-std::unique_ptr<Node> parsePrint(const std::vector<lexer::Token>& tokens, int& pos);
-std::unique_ptr<Node> parseIdentifier(const std::vector<lexer::Token>& tokens, int& pos);
-std::unique_ptr<Node> parseIf(const std::vector<lexer::Token>& tokens, int& pos);
-std::unique_ptr<Node> parseConditional(const std::vector<lexer::Token>& tokens, int& pos);
-std::unique_ptr<Node> parseExpression(const std::vector<lexer::Token>& tokens, int& pos);
-std::unique_ptr<Node> parseTerm(const std::vector<lexer::Token>& tokens, int& pos);
-std::unique_ptr<Node> parseFactor(const std::vector<lexer::Token>& tokens, int& pos);
-void printAST(const std::unique_ptr<Node>& node, int indent = 0);
-}  // namespace parser
+class Parser {
+   private:
+    int tokenPosition = 0;
+    int lineNumber = 0;
+    int tokenLength = 0;
+
+   public:
+    static void printAST(const std::unique_ptr<Node>& node, int indent = 0);
+    std::unique_ptr<Node> parseProgram(const std::vector<Token>& tokens);
+
+   private:
+    std::unique_ptr<Node> parseStatement(const std::vector<Token>& tokens);
+    std::unique_ptr<Node> parsePrint(const std::vector<Token>& tokens);
+    std::unique_ptr<Node> parseIdentifier(const std::vector<Token>& tokens);
+    std::unique_ptr<Node> parseIf(const std::vector<Token>& tokens);
+    std::unique_ptr<Node> parseConditional(const std::vector<Token>& tokens);
+    std::unique_ptr<Node> parseExpression(const std::vector<Token>& tokens);
+    std::unique_ptr<Node> parseTerm(const std::vector<Token>& tokens);
+    std::unique_ptr<Node> parseFactor(const std::vector<Token>& tokens);
+};
