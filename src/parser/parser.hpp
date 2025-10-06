18def31b088fc234721c3df14cc88f92a1199597
Kieroxide <kieranbprivate:gmail.com>
2025-10-06 20:24:55 +0100

    Implemented deep cloning for Node structure and refined function declarations in Parser class

--- src/parser/parser.hpp
+++ src/parser/parser.hpp
@@ -45,6 +45,23 @@ struct Node {
         this->token = token;
         this->value = value;
     }
+
+    Node* clone() const {
+        // Create a new node with the same properties
+        Node* newNode = new Node();
+        newNode->type = this->type;
+        newNode->value = this->value;
+        newNode->token = this->token;
+
+        // Clone all children recursively
+        for (const auto& child : this->children) {
+            if (child) {
+                newNode->children.push_back(std::unique_ptr<Node>(child->clone()));
+            }
+        }
+
+        return newNode;
+    }
 };
 
 class Parser {
@@ -58,9 +75,10 @@ class Parser {
     std::unique_ptr<Node> parseProgram(const std::vector<Token>& tokens);
 
    private:
-    std::unique_ptr<Node> Parser::parseFunction(const std::vector<Token>& tokens);
-    std::unique_ptr<Node> Parser::parseFunctionCall(const std::vector<Token>& tokens);
-    std::unique_ptr<Node> Parser::parseParams(const std::vector<Token>& tokens, std::unique_ptr<Node>& funcNode);
+    void parseParams(const std::vector<Token>& tokens, std::unique_ptr<Node>& funcNode);
+
+    std::unique_ptr<Node> parseFunction(const std::vector<Token>& tokens);
+    std::unique_ptr<Node> parseFunctionCall(const std::vector<Token>& tokens);
     std::unique_ptr<Node> parseStatement(const std::vector<Token>& tokens);
     std::unique_ptr<Node> parsePrint(const std::vector<Token>& tokens);
     std::unique_ptr<Node> parseIdentifier(const std::vector<Token>& tokens);
