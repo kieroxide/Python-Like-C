2e224d6ad89a404c3d672c2c12a89f5f46647b65
Kieroxide <kieranbprivate:gmail.com>
2025-10-06 21:15:14 +0100

    Revamped tests to return values and for test script to check for correct output

--- tests/src/runTests.cpp
+++ tests/src/runTests.cpp
@@ -1,22 +1,39 @@
-#include <cstdlib>
 #include <iostream>
 #include <string>
 #include <vector>
 
-#include "src/Executor/Executor.hpp"
+#include "src/Executor/executor.hpp"
 
 using namespace std;
 
+struct TestCase {
+    std::string filename;
+    int expected;
+};
+
 int main() {
     string testsDir = "tests/";
-    vector<string> tests = {"test_simple_assign.txt", "test_arith.txt", "test_conditionals.txt", "test_nested.txt"};
+    vector<TestCase> tests = {{"test_simple_assign.txt", 12}, {"test_arith.txt", 44},
+                              {"test_conditionals.txt", 11},  {"test_nested.txt", 102},
+                              {"test_functions.txt", 208},    {"test_scope.txt", 660}};
 
-    for (auto const& t : tests) {
-        cout << "=== Running " << t << " ===\n";
-        int response = Executor::executeFile(testsDir + t);
-        if (response != 0)
-            cerr << "Test exited with code " << response << "\n";
-        cout << '\n' << flush;
+    bool allPassed = true;
+    for (const auto& test : tests) {
+        cout << "=== Running " << test.filename << " ===\n";
+        int result = executor::executeFile(testsDir + test.filename);
+        cout << "Returned: " << result << ", Expected: " << test.expected << "\n";
+        if (result != test.expected) {
+            cout << "Test FAILED!\n";
+            allPassed = false;
+        } else {
+            cout << "Test passed.\n";
+        }
+        cout << endl;
     }
-    return 0;
+    if (allPassed) {
+        cout << "All tests passed!\n";
+    } else {
+        cout << "Some tests failed.\n";
+    }
+    return allPassed ? 0 : 1;
 }
