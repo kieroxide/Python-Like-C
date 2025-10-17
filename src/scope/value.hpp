f1f3700a60be9a39980c414e2b46650ea2375b90
Kieroxide <kieranbprivate:gmail.com>
2025-10-17 16:29:07 +0100

    Removed unnessarcy use of Bool in value types. i'll just use int for simplicity

--- src/scope/value.hpp
+++ src/scope/value.hpp
@@ -6,17 +6,14 @@ struct Value;
 using Array = std::vector<Value>;
 
 struct Value {
-    std::variant<int, Array, bool> v;
+    std::variant<int, Array> v;
     Value() : v(0) {}
     Value(int i) : v(i) {}
-    Value(bool b) : v(b) {}
     Value(const Array& a) : v(a) {}
 
     bool isInt() const { return std::holds_alternative<int>(v); }
-    bool isBool() const { return std::holds_alternative<bool>(v); }
     bool isArray() const { return std::holds_alternative<Array>(v); }
     int asInt() const { return std::get<int>(v); }
-    bool asBool() const { return std::get<bool>(v); }
     Array& asArray() { return std::get<Array>(v); }
     const Array& asArray() const { return std::get<Array>(v); }
 };
